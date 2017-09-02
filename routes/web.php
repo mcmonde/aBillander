b0a65f00fd35f58616eedffeb740f4c69ede3f4f
Lara Billander <wasp@abillander.com>
2017-09-02 13:55:30 +0200

    Done: php artisan make:auth

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,26 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Auth::routes();
+Route::group(['middleware' => ['web']], function() {
+
+// Login Routes...
+    Route::get('login', ['as' => 'login', 'uses' => 'Auth\LoginController@showLoginForm']);
+    Route::post('login', ['uses' => 'Auth\LoginController@login']);
+    Route::post('logout', ['as' => 'logout', 'uses' => 'Auth\LoginController@logout']);
+
+// Registration Routes...
+    Route::get('register', ['as' => 'register', 'uses' => 'Auth\RegisterController@showRegistrationForm']);
+    Route::post('register', ['uses' => 'Auth\RegisterController@register']);
+
+// Password Reset Routes...
+    Route::post('password/email', ['as' => 'password.email', 'uses' => 'Auth\ForgotPasswordController@sendResetLinkEmail']);
+    Route::get('password/reset', ['as' => 'password.request', 'uses' => 'Auth\ForgotPasswordController@showLinkRequestForm']);
+    Route::post('password/reset', ['uses' => 'Auth\ResetPasswordController@reset']);
+    Route::get('password/reset/{token}', ['as' => 'password.reset', 'uses' => 'Auth\ResetPasswordController@showResetForm']);
+});
+
+
+Route::get('/home', 'HomeController@index')->name('home');
